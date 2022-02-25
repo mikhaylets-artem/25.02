72e8f4b0ab0bfe71191f8052bf605fccc82509db
unknown <artems.mikhaylets@gmail.com>
2022-02-25 19:47:21 +0200

    g

---
+++ boob/main.py
@@ -0,0 +1,172 @@
+import math
+#1
+#s=0
+#j=int(input("че хотите?1-площадь\2-длинна"))
+#if j==1:
+#    r1=int(input('r= '))
+#    s=math.pi*r1**2
+#    print(s)
+#elif j==2:
+#    r2=int(input())
+#    print(2*math.pi*r2)
+#2
+#a=(input("password: "))
+#if a=="jojo":
+#    print("krasava")
+#else:
+#    print("ne krasava")
+#3
+#while True:
+#    age=int(input("возраст: "))
+#    hands=int(input("склько у вас рук: "))
+#    name=input("как вас зовут: ")
+#    cash=int(input("skoka nado"))
+#    if age<64 or hands <=2 or name!='jojo' and cash!=228:
+#        print("sorrую,но сегодня без кредита")
+#        print("novaya operacya! ")
+#        continue
+#    else:
+#        print('dobre,skoro dengi budut vashi')
+#        print("novaya operacya! ")
+#        continue
+
+
+#4
+#n=int(input("ocneka"))
+#if n==1 or n==2 or n==3 or n==4 or n==5 or n==6 :
+#        print("loh")
+#elif n==7 or n==8 or n==9:
+#        print("pod pivo poydet")
+#elif n==10 or n==11 or n==12:
+#       print("krasava")
+#else:
+#      print("не ври мне")
+#5
+#z=float(input("z= "))
+#x=float(input("x= "))
+#y=float(input("y= "))
+#if z==math.sqrt(x*y) and x>y:
+#    print(z)
+#else:
+#    z=math.log(x+y)
+#    print(z)
+#6
+#t=int(input())
+#if t>20:
+#    print("on")
+#elif t<=20:
+#    print("off")
+#7
+#n=int(input("n"))
+#if n%2==0:
+#    print("2")
+#elif n%2!=0:
+#    print("1")
+#8
+#a=int(input("a="))
+#b=int(input("b="))
+#c=int(input("c="))
+#if a + b > c and a + c > b and b + c > a:
+#    print("Треугольник существует")
+#else:
+#    print("Треугольник не существует")
+#9
+#x=int(input())
+#y=int(input())
+#if x>0 and y>0  :
+#   print("1")
+#elif x< 0 and y>0 :
+#    print("2")
+#elif x<0 and y<0 :
+#    print("3")
+#elif x>0 and y<0 :
+#   print("4`")
+#10
+#x=int(input())
+#if x>0:
+#    y=2*x-10
+#    print(y)
+#elif x==0:
+#    y=0
+#    print(y)
+#else:
+#    y=2*abs(x)-1
+#    print(y)
+#mar grebany
+#1
+#x=float(input("x= "))
+#if x>=0:
+#    y=x+5
+#    print(y)
+#2
+#x=float(input("x= "))
+#if x!=7:
+#    y=1/x-7
+#    print(y)
+#3
+# x=float(input("x= "))
+# if x!=-5:
+#     y=1/math.sqrt(x+5)
+#     print(y)
+#4
+# x=float(input("x= "))
+# if abs(x+5)!=0:
+#     y=1/abs(x+5)
+#     print(y)
+#5
+# x=float(input("x= "))
+# if abs(x+5)!=0:
+#     y=1/abs(x+5)
+#     print(y)
+#6
+# x=float(input("x= "))
+# if 5-x>=0 and x-7>=0:
+#     y=math.sqrt(x+5)+math.sqrt(x-7)
+#     print(y)
+#7
+# x=float(input("x= "))
+# if 5-x>=0 and x-7!=0:
+#     y=math.sqrt(x+5)+1/x-7
+#     print(y)
+#8
+#x=float(input("x= "))
+#if x+5>0 and x-7!=0:
+#  y=math.sqrt(x+5)+1/x-7
+#  print(y)
+#kapec
+#1
+# x=float(input("x= "))
+#
+# if x>0:
+#     y=x
+#     print(y)
+# else:
+#     y=x**2
+#     print(y)
+#2
+# x=float(input("x= "))
+# if -10<x<5:
+#     y=x
+#     print(y)
+# else:
+#     y=x**2
+#     print(y)
+#3
+# x=float(input("x= "))
+# if (x>=1 and x<=5) or (x>=10 and x>10):
+#     y=x
+#     print(y)
+# else:
+#     y=x**2
+#     print(y)
+# #4
+# x=float(input("x= "))
+# if x<=0:
+#     y=x
+#     print(y)
+# elif x>0 and x<=5:
+#     y=x**2
+#     print(y)
+# else:
+#     y=25
+#     print(y)
